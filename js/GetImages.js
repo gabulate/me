70bf496d8fd4cf75a696edb295ca0eccd5d0ef04
gabulate <gabrielulate71@gmail.com>
2023-05-18 15:32:16 -0600

    Reworked GetImages script again again (Now it uses an asynchronous request in a recursive method 🤯)

--- js/GetImages.js
+++ js/GetImages.js
@@ -1,27 +1,35 @@
 
 var folder = "img/gallery/";
-var i = 1;
-while (i <= 30) {
 
-    var http = new XMLHttpRequest()
-    http.open('HEAD', folder + i + ".jpg", false)
-    http.send()
-    if (http.status === 200) {
+function LoadImages (i) {
 
-        $("#gallery").append(
-            "<div class='col-md-auto'>" +
-            "<div class='thumbnail' data-bs-toggle='modal' data-bs-target='#galleryModal'>" +
-            "<img src='" + folder + i + ".jpg" + "' data-bs-target='#carousel' data-bs-slide-to='0'>" +
-            "</div>" +
-            "</div>"
-        );
-    } else {
-        i = 99999;
-    }
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', folder + i + ".jpg", true)
 
-    i++;
+    xhr.onload = (e) => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            $("#gallery").append(
+                "<div class='col-md-auto'>" +
+                "<div class='thumbnail' data-bs-toggle='modal' data-bs-target='#galleryModal'>" +
+                "<img src='" + folder + i + ".jpg" + "' data-bs-target='#carousel' data-bs-slide-to='0'>" +
+                "</div>" +
+                "</div>"
+            );
+            
+            LoadImages(i+1)
+          } else {
+            ShowImage();
+          }
+        }
+      };
+      xhr.onerror = (e) => {
+        
+      };
+
+    xhr.send(null);
 }
-
+LoadImages(1);
 
 function ShowImage() {
     const highlight = document.querySelector('.carouselImg');
@@ -36,5 +44,4 @@ function ShowImage() {
     })
 }
 
-ShowImage();
 
